1fdbf304335fecf6e3a57fb5ed10294c2721507e
nerdeveloper <odirionye@gmail.com>
2018-06-17 01:46:39 +0100

    chore(dataservice): add a delete feature

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,6 +3,7 @@ import { DataService } from '../data.service';
 import { Coffee } from '../logic/coffee';
 import { Router } from '@angular/router';
 import { GeolocationService } from '../geolocation.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list',
@@ -12,13 +13,20 @@ import { GeolocationService } from '../geolocation.service';
 export class ListComponent implements OnInit {
   list: [Coffee];
 
-  constructor(private data: DataService, private router: Router, private geolocation: GeolocationService) { }
+  constructor(private snackBar: MatSnackBar, private data: DataService, private router: Router, private geolocation: GeolocationService) { }
   getDetails(coffee: Coffee) {
     this.router.navigate(['/coffee', coffee._id]);
   }
   getMap(coffee) {
   const mapurl = this.geolocation.getMapLink(coffee.location);
   location.href  = mapurl;
+}
+onDelete(coffee: Coffee) {
+  this.data.delete(coffee, result => {
+     this.snackBar.open('Message Deleted');
+     const INDEX = this.list.indexOf(coffee);
+     this.list.splice(INDEX, 1);
+  });
 }
 share(coffee: Coffee) {
   const shareText = `I had this coffee at ${coffee.name} and for me it's a ${coffee.rating} for me`;
